e1f9874c257b1e9c67a43fae30b57f40ba5d5ad5
Rettera <nathan.rioux23@gmail.com>
2019-04-11 09:44:37 +0200

    Meteor-Base

---
+++ both/collections.js
@@ -0,0 +1,12 @@
+Notes = new Mongo.Collection('notes'); 
+Notes.allow({ 
+    insert: function() { 
+        return true; 
+    }, 
+    update: function() { 
+        return true; 
+    }, 
+    remove: function() { 
+        return true; 
+    } 
+});
---
+++ client/main.js
@@ -0,0 +1,57 @@
+
+Template.form_new_note.events({ 
+  "submit .js-new-note"(event,instance) { 
+    event.preventDefault();
+
+     const titleVal = event.target.title.value;
+     const textVal = event.target.text.value;
+
+
+     Notes.insert({
+        title: titleVal,
+        text: textVal,
+        createdAt: new Date(),
+        ownerId: Meteor.userId()
+     });
+     event.target.title.value = "";
+     event.target.text.value = "";
+  } 
+}); 
+
+Template.list_note.helpers({
+  notes() {
+    return Notes.find({ownerId: Meteor.userId()}).fetch();
+  }
+});
+
+Template.single_note.events({ 
+  'click .js-edit-note': function(event, instance) { 
+     
+
+     Modal.show('modal_edit_note', instance.data);
+  } 
+}); 
+Template.modal_edit_note.events({ 
+  "submit .js-edit-note"(event,instance) { 
+    event.preventDefault();
+    const idNote = instance.data.note._id;
+    Notes.update({_id: idNote},{$set: {
+      title: event.target.title.value,
+      text: event.target.text.value
+    }
+    });
+    Modal.hide();
+  }, 
+  "click .js-delete-note" (event, instance) {
+  Notes.remove({_id: instance.data.note._id});
+  Modal.hide();
+  }
+}); 
+
+Template.accueil.events({ 
+  'click .js-logout': function(event, instance) { 
+     
+
+     Meteor.logout();
+  } 
+});
